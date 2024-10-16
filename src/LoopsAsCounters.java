ad1b03210c4aad17ebd1cc837da1305e00b4dccc
Zephyros <zxphyros@gmail.com>
2024-10-16 08:36:27 -0400

    *mario kart double dash announcer voice* finish!!

--- src/LoopsAsCounters.java
+++ src/LoopsAsCounters.java
@@ -1,8 +1,34 @@
 public class LoopsAsCounters {
     public static void run(){
-        final int BOUNDS = 5;
-        for(int i = 0; i>BOUNDS; i++){
-            for(int i )
+        StringBuilder result = new StringBuilder();
+        //count to 30
+        for(int i = 0; i<=30; i++){
+            result.append(i).append(" "); //appends current num and a space to the result
         }
+        System.out.println(result);
+
+        result = new StringBuilder(); //clears stringbuilder
+
+        //count down from 30
+        for(int i = 30; i>=0; i--){
+            result.append(i).append(" "); //appends current num and a space to the result
+        }
+        System.out.println(result);
+        result = new StringBuilder(); //clears stringbuilder
+
+
+        //count to 18 by 3
+        for(int i = 0; i<=18; i+=3){
+            result.append(i).append(" "); //appends current num and a space to the result
+        }
+        System.out.println(result);
+        result = new StringBuilder(); //clears stringbuilder
+
+        //count down from 10
+        for(int i = 10; i>=0; i-=2){
+            result.append(i).append(" "); //appends current num and a space to the result
+        }
+        System.out.println(result);
+
     }
 }
--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,5 +1,29 @@
 public class NestedLoops {
     public static void run(){
+        //init var
+        final int BOUNDS = 5;
+        final String RESULT = "*****";
+        String resultSubstring;
 
+        //asterisks ascending
+        for(int i = 0; i <BOUNDS; i++){
+            resultSubstring = RESULT.substring((BOUNDS-i)-1);
+            System.out.println(resultSubstring);
+        }
+
+        System.out.println(); //Buffer to space out new lines
+
+        //asterisks descending
+        for(int i = 0; i <BOUNDS; i++){
+            resultSubstring = RESULT.substring(i);
+            System.out.println(resultSubstring);
+        }
+
+        System.out.println(); //Buffer to space out new lines
+
+        //box of asterisks
+        for(int i = 0; i<BOUNDS; i++){
+            System.out.println(RESULT);
+        }
     }
 }
